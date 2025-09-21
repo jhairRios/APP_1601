103d3b55b7db98aac7c438b132133cdbb8fe8f3b
EDSON JHAIR RIOS COTO <ejrios@uth.hn>
2025-09-21 15:50:04 -0600

    Ruta Jhair

--- flutter_application_1/lib/screens/login_screen.dart
+++ flutter_application_1/lib/screens/login_screen.dart
@@ -63,9 +63,9 @@ class _LoginScreenState extends State<LoginScreen> {
       final response = await http.post(
         Uri.parse(
           //'http://localhost/APP_1601/flutter_application_1/php/api.php' //Ruta Diany Enamorado
-          'http://localhost/Aplicacion_1/APP1601/APP_1601/flutter_application_1/php/api.php' //Ruta Angel Perez
-          // //Ruta Jhair Rios
-          // //Ruta Derick Muñoz, 
+          //'http://localhost/Aplicacion_1/APP1601/APP_1601/flutter_application_1/php/api.php' //Ruta Angel Perez
+          'http://localhost/Proyecto_APP/Proyecto_APP/flutter_application_1/php/api.php', //Ruta Jhair Rios
+          // //Ruta Derick Muñoz,
         ),
         body: {
           'email': _emailController.text.trim(),
