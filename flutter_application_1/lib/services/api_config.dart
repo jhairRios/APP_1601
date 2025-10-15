1a5be53e507f75828b19d55b229118ea87c01367
AngelPerez557 <aaperez@uth.hn>
2025-10-15 16:49:32 -0600

    Prueba Ngrok

--- flutter_application_1/lib/services/api_config.dart
+++ flutter_application_1/lib/services/api_config.dart
@@ -4,8 +4,7 @@
 // Modo híbrido (desarrollo local + BD en AWS): apunta al PHP que correrás en Laragon/Apache.
 // Usa esta URL mientras desarrollas localmente. Cuando despliegues PHP en un servidor público,
 // reemplaza este valor por la URL pública (https://mi-servidor/.../php/api.php).
-const String API_BASE_URL =
-    'http://localhost/Aplicacion_1/APP1601/APP_1601/flutter_application_1/php/api.php';
+const String API_BASE_URL = 'https://4dc1d0fd4725.ngrok-free.app/Aplicacion_1/APP1601/APP_1601/flutter_application_1';
 // Ejemplos de uso:
 // Uri.parse('$API_BASE_URL?action=get_roles')
 // Uri.parse(API_BASE_URL)
