bf3170b5e1cf91fa12a678b424df0ff392b78b41
AngelPerez557 <aaperez@uth.hn>
2025-11-18 15:52:16 -0600

    Ultimo

--- flutter_application_1/android/app/build.gradle.kts
+++ flutter_application_1/android/app/build.gradle.kts
@@ -23,7 +23,7 @@ if (keystorePropertiesFile.exists()) {
 
 
 android {
-    namespace = "com.example.flutter_application_1"
+    namespace = "com.app.pedidos1601"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -38,7 +38,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_application_1"
+        applicationId = "com.app.pedidos1601"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
